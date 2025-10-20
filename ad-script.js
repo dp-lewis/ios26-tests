00c748854412a1978263fbb3b43ddca55ace8cfa
David Lewis <mail@david-lewis.com>
2025-10-20 20:34:57 +1100

    Refactor ad-script.js and update index.html to use the correct script file; remove obsolete script.js

--- ad-script.js
+++ ad-script.js
@@ -1,8 +1,15 @@
 const callbacks = [];
 
+const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
 runCallbacks = () => {
     callbacks.forEach(cb => cb());
 };
+
+function trackAdDimensions() {
+    console.log(`Ad dimensions - Width: ${width}, Height: ${height}`);
+}
 
 window.addEventListener('load', () => {
     console.log('Document loaded.');
